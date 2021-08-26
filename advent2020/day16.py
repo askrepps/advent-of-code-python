810df13347fc67f19073d546b68520a5377ffb7c
askrepps <askrepps@gmail.com>
2021-08-26 17:14:17 -0400

    Added solutions for part 2 of day 16

--- advent2020/day16.py
+++ advent2020/day16.py
@@ -61,30 +61,80 @@ def parse_input(lines):
     return field_ranges_by_name, my_ticket, nearby_tickets
 
 
-def is_value_invalid(value, field_ranges_by_name):
-    for range_list in field_ranges_by_name.values():
-        for field_range in range_list:
-            if field_range[0] <= value <= field_range[1]:
-                return False
+def is_value_always_invalid(value, field_ranges_by_name):
+    for field_ranges in field_ranges_by_name.values():
+        if is_value_valid_for_ranges(value, field_ranges):
+            return False
     return True
 
 
-def get_part1_answer(lines):
-    field_ranges_by_name, _, nearby_tickets = parse_input(lines)
+def is_value_valid_for_ranges(value, field_ranges):
+    for field_range in field_ranges:
+        if field_range[0] <= value <= field_range[1]:
+            return True
+    return False
 
+
+def find_field_positions(field_ranges_by_name, valid_tickets):
+    fields = field_ranges_by_name.keys()
+    num_fields = len(fields)
+    valid_fields_by_position = [[field for field in fields] for _ in range(num_fields)]
+    for ticket in valid_tickets:
+        for position, value in enumerate(ticket):
+            fields_copy = valid_fields_by_position[position].copy()
+            for field in fields_copy:
+                if not is_value_valid_for_ranges(value, field_ranges_by_name[field]):
+                    valid_fields_by_position[position].remove(field)
+
+    cleaning_up = True
+    cleanup_keys = set()
+    while cleaning_up:
+        cleaning_up = False
+        for i, valid_fields in enumerate(valid_fields_by_position):
+            if len(valid_fields) == 1:
+                valid_field = valid_fields[0]
+                if valid_field not in cleanup_keys:
+                    cleaning_up = True
+                    cleanup_keys.add(valid_field)
+                    for j, cleanup_list in enumerate(valid_fields_by_position):
+                        if i != j and valid_field in cleanup_list:
+                            cleanup_list.remove(valid_field)
+
+    field_positions = {}
+    for position, fields in enumerate(valid_fields_by_position):
+        if len(fields) != 1:
+            raise RuntimeError(f"{len(fields)} fields are valid for ticket position {position}")
+        field_positions[fields[0]] = position
+
+    return field_positions
+
+
+def get_part1_answer_and_filter_valid_tickets(field_ranges_by_name, nearby_tickets):
     result = 0
+    valid_tickets = []
     for ticket in nearby_tickets:
-        for field in ticket:
-            if is_value_invalid(field, field_ranges_by_name):
-                result += field
+        is_valid = True
+        for value in ticket:
+            if is_value_always_invalid(value, field_ranges_by_name):
+                result += value
+                is_valid = False
+        if is_valid:
+            valid_tickets.append(ticket)
+    return result, valid_tickets
+
+
+def get_part2_answer(field_ranges_by_name, my_ticket, valid_tickets):
+    result = 1
+    field_positions = find_field_positions(field_ranges_by_name, valid_tickets)
+    for field in field_positions.keys():
+        if field.startswith("departure"):
+            result *= my_ticket[field_positions[field]]
     return result
-
-
-def get_part2_answer(lines):
-    return None
 
 
 def run():
     lines = util.get_input_file_lines("day16.txt")
-    print(f"The answer to part 1 is {get_part1_answer(lines)}")
-    print(f"The answer to part 2 is {get_part2_answer(lines)}")
+    field_ranges_by_name, my_ticket, nearby_tickets = parse_input(lines)
+    part1_answer, valid_tickets = get_part1_answer_and_filter_valid_tickets(field_ranges_by_name, nearby_tickets)
+    print(f"The answer to part 1 is {part1_answer}")
+    print(f"The answer to part 2 is {get_part2_answer(field_ranges_by_name, my_ticket, valid_tickets)}")
--- test/test_day16.py
+++ test/test_day16.py
@@ -23,12 +23,13 @@
 
 import unittest
 
-from advent2020.day16 import get_part1_answer
+from advent2020.day16 import get_part1_answer_and_filter_valid_tickets
 from advent2020.day16 import get_part2_answer
+from advent2020.day16 import parse_input
 from advent2020.util import get_input_data_lines
 
 
-data = """
+data1 = """
 class: 1-3 or 5-7
 row: 6-11 or 33-44
 seat: 13-40 or 45-50
@@ -43,9 +44,30 @@ nearby tickets:
 38,6,12
 """
 
+data2 = """
+class: 0-1 or 4-19
+departure row: 0-5 or 8-19
+departure seat: 0-13 or 16-19
+
+your ticket:
+11,12,13
+
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9
+"""
+
 
 class Day16Test(unittest.TestCase):
-    def test_day16(self):
-        lines = get_input_data_lines(data)
-        self.assertEqual(get_part1_answer(lines), 71)
-        self.assertEqual(get_part2_answer(lines), None)
+    def test_day16_part1(self):
+        lines = get_input_data_lines(data1)
+        field_ranges_by_name, _, nearby_tickets = parse_input(lines)
+        part1_answer, _ = get_part1_answer_and_filter_valid_tickets(field_ranges_by_name, nearby_tickets)
+        self.assertEqual(part1_answer, 71)
+
+    def test_day16_part2(self):
+        lines = get_input_data_lines(data2)
+        field_ranges_by_name, my_ticket, nearby_tickets = parse_input(lines)
+        _, valid_tickets = get_part1_answer_and_filter_valid_tickets(field_ranges_by_name, nearby_tickets)
+        self.assertEqual(get_part2_answer(field_ranges_by_name, my_ticket, valid_tickets), 143)
